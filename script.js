0a7c3a236878520aa354eceb41625f6924da31b3
A.Westwood <personal.email.aw1@gmail.com>
2024-06-10 13:26:40 +0100

    09. add password min and max

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ form.addEventListener('submit', (e) => {
         messages.push('Name is required');
     };
 
+    if (password.value.length <= 6 || password.value.length >= 20) {
+        messages.push('Password must be 6-20 characters');
+    };
+
+
     if(messages.length > 0) {
         e.preventDefault();
         errorElement.innerText = messages.join(', ');
